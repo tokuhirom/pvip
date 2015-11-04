6a82565a38a8d8026e418323479aefc399d84e6a
Tokuhiro Matsuno <tokuhirom@gmail.com>
2015-11-05 00:08:44 +0900

    fixed memory related bugs

--- src/pvip_string.c
+++ src/pvip_string.c
@@ -14,7 +14,7 @@ PVIPString *PVIP_string_new() {
     }
     memset(str, 0, sizeof(PVIPString));
     str->len    = 0;
-    str->buflen = 1024;
+    str->buflen = 256;
     str->buf    = malloc(str->buflen);
     if (!str->buf) {
         fprintf(stderr, "[PVIP] Cannot allocate memory\n");
@@ -44,13 +44,13 @@ PVIP_BOOL PVIP_string_concat(PVIPString *str, const char *src, size_t len) {
 
 PVIP_BOOL PVIP_string_concat_int(PVIPString *str, int64_t n) {
     char buf[1024];
-    int res = snprintf(buf, 1023, "%" PRIi64, n);
+    int res = snprintf(buf, sizeof(buf), "%" PRIi64, n);
     return PVIP_string_concat(str, buf, res);
 }
 
 PVIP_BOOL PVIP_string_concat_number(PVIPString *str, double n) {
     char buf[1024];
-    int res = snprintf(buf, 1023, "%f", n);
+    int res = snprintf(buf, sizeof(buf), "%f", n);
     for (; res>1; --res) { /* remove trailing zeros */
         if (buf[res-1]!='0') {
             break;
@@ -70,13 +70,13 @@ void PVIP_string_say(PVIPString *str) {
 
 PVIP_BOOL PVIP_string_vprintf(PVIPString *str, const char*format, va_list ap) {
     int size = vsnprintf(str->buf + str->len, str->buflen - str->len, format, ap);
-    if (size > str->buflen - str->len) {
+    if (size+1 > str->buflen - str->len) {
         str->buflen += size + 1;
         str->buf    = realloc(str->buf, str->buflen);
         if (!str->buf) {
             return PVIP_FALSE;
         }
-        return PVIP_string_printf(str, format, ap);
+        return PVIP_string_vprintf(str, format, ap);
     }
     str->len += size;
     return PVIP_TRUE;
